7112dae3ea086664cdb783fb7bc259feaa077d40
mansion <1547246193@qq.com>
2021-11-16 11:47:33 +0800

    update some code.

--- mimple.py
+++ mimple.py
@@ -1,25 +1,25 @@
-import smtplib
+import test
 from email.mime.text import MIMEText
 from email.header import Header
 # 第三方 SMTP 服务
 mail_host = "smtp.qq.com"  # 设置服务器
 mail_user = "yifouforum@qq.com" # 用户名
 mail_pass = "frgpfojetmifhjeg" # 口令
-sender = "yifouforum@qq.com"
 # receivers = ['21210240375@m.fudan.edu.cn'] # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
-receivers = ['19307110217@fudan.edu.cn']
-message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
-message['From'] = Header("python robot", 'utf-8')
-message['To'] = Header("google mail receiver", 'utf-8')
+receivers = ['1547246193@qq.com']
+message = MIMEText('你好帅我好爱\r\n哈哈哈你好\r\n', 'plain', 'utf-8')
+message['From'] = Header(mail_user, 'utf-8')
+message['To'] = Header((', ').join(receivers), 'utf-8')
 subject = 'Python SMTP 邮件测试'
 message['Subject'] = Header(subject, 'utf-8')
 try:
-    smtp = smtplib.SMTP()
+    smtp = test.SMTP()
+    smtp.debuglevel = 1
     smtp.connect(mail_host, 587)
     smtp.login(mail_user, mail_pass)
-    smtp.sendmail(sender, receivers, message.as_string())
-    print(message.as_string())
+    smtp.sendmail(mail_user, receivers, message.as_string())
+    # print(message.as_string())
     print('发送成功')
-except smtplib.SMTPException as e:
+except test.SMTPException as e:
     print('发送失败')
     raise e
--- simple.py
+++ simple.py
@@ -1,14 +1,25 @@
 import ksmtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 mail_host = "smtp.qq.com"  
 mail_user = "yifouforum@qq.com" 
 mail_pass = "frgpfojetmifhjeg" 
-sender = "yifouforum@qq.com"
+
+receivers = ['1547246193@qq.com']
+message = MIMEText('你好帅我好爱\r\n哈哈哈你好\r\n', 'plain', 'utf-8')
+message['From'] = Header(mail_user, 'utf-8')
+message['To'] = Header((', ').join(receivers), 'utf-8')
+subject = 'KSMTP test'
+message['Subject'] = Header(subject, 'utf-8')
 
 try:
-    smtp = ksmtplib.SMTP(1)
-    smtp.connect(mail_host, 587)
-    smtp.login(mail_user, mail_pass)
+    ksmtp = ksmtplib.KSMTP(1)
+    ksmtp.connect(mail_host, 587)
+    ksmtp.login(mail_user, mail_pass)
+    ksmtp.email(mail_user, receivers, message.as_string())
+    ksmtp.close()
 except OSError as e:
     print('发送失败')
     raise e
+    
