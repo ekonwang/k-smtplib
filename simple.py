ea5b39a6d2443592d7c97775f5082284f73b0047
mansion <1547246193@qq.com>
2021-11-17 11:42:02 +0800

    make it clean.

--- simple.py
+++ simple.py
@@ -1,36 +1,36 @@
-from datetime import datetime
 import ksmtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-current_time_stamp = str(datetime.now())
 
-mail_host = "smtp.qq.com"  
-mail_user = "yifouforum@qq.com" 
-mail_pass = "frgpfojetmifhjeg" 
+mail_host = None # For instance : "smtp.qq.com".
+mail_user = "yifouforum@qq.com" # Your acount, for instance, "1@qq.com".
+mail_pass = "frgpfojetmifhjeg" # Your password.
 
-content = f'''
+content =  None # Content of email, for instance:
+f'''
 神秘博士：
     您好，这里是M78星云给您发来的问候。
 此致
 '''
 
-receivers = ['g1547246193@gmail.com']
+receivers = None # A list of receiver, for instance : ['g1547246193@gmail.com']
 message = MIMEText(content, 'plain', 'utf-8')
 message['From'] = Header(mail_user, 'utf-8')
 message['To'] = Header((', ').join(receivers), 'utf-8')
-subject = 'KSMTP Test %s' %current_time_stamp
+subject = None # Email subject: 'KSMTP Test' 
 message['Subject'] = Header(subject, 'utf-8')
 
 try:
     ksmtp = ksmtplib.KSMTP(1)
-    ksmtp.connect(mail_host, 587)
+    port = None # A port.
+    ksmtp.connect(mail_host, port)
     ksmtp.start_tls()
     ksmtp.login(mail_user, mail_pass)
     ksmtp.email(mail_user, receivers, message.as_string())
     ksmtp.close()
-    print('发送成功')
+    print('Sent Successfully.')
 except OSError as e:
-    print('发送失败')
+    print('Failed to send.')
     raise e
     
