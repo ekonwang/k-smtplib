5410693886cd39c79eddff0f64c7a8582522cb09
mansion <1547246193@qq.com>
2021-11-17 11:21:26 +0800

    mimple.py updated

--- mimple.py
+++ mimple.py
@@ -2,19 +2,25 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 # 第三方 SMTP 服务
+content = f'''
+神秘博士：
+    您好，这里是M78星云给您发来的问候。
+此致
+'''
+
 mail_host = "smtp.qq.com"  # 设置服务器
 mail_user = "yifouforum@qq.com" # 用户名
-mail_pass = "frgpfojetmifhjeg" # 口令
-# receivers = ['21210240375@m.fudan.edu.cn'] # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+mail_pass = "frgpfojetmifhjeg" # 口令
+# receivers = ['21210240375@m.fudan.edu.cn'] # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 receivers = ['g1547246193@gmail.com']
-message = MIMEText('你好帅我好爱\r\n哈哈哈你好\r\n', 'plain', 'utf-8')
+message = MIMEText(content, 'plain', 'utf-8')
 message['From'] = Header(mail_user, 'utf-8')
 message['To'] = Header((', ').join(receivers), 'utf-8')
 subject = 'Python SMTP 邮件测试'
 message['Subject'] = Header(subject, 'utf-8')
 try:
     smtp = smtplib.SMTP(host=mail_host, port=587)
-    smtp.debuglevel = 1
+    smtp.debuglevel = 0
     smtp.connect(mail_host, port=587)
     smtp.starttls()
     smtp.login(mail_user, mail_pass)
